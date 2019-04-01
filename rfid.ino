136c5b13414739bf5a86e461e00f9991e431a22e
jparedesf <49141980+jparedesf@users.noreply.github.com>
2019-04-01 01:32:00 -0500

    Update rfid.ino

--- rfid.ino
+++ rfid.ino
@@ -1,4 +1,5 @@
 
+//Importacion de librerias
 #include <SPI.h>
 #include <MFRC522.h>
 
@@ -12,16 +13,20 @@ void setup() {
   mfrc522.PCD_Init(); // Iniciamos  el MFRC522
 }
 
+//proceso que se ejecutara de manera continua
 void loop() {
+  //se comprueba de que haya conectada un lector RFID al arduino
   if ( ! mfrc522.PICC_IsNewCardPresent())
         return;
     if ( ! mfrc522.PICC_ReadCardSerial())
         return;
+    //llama al método para leer el codigo de la tarjeta RFID
     dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
     mfrc522.PICC_HaltA();
     mfrc522.PCD_StopCrypto1();
 }
 
+//método que lee el codigo de la tarjeta RFID y lo muestra por el cable serial
 void dump_byte_array(byte *buffer, byte bufferSize) {
     for (byte i = 0; i < bufferSize; i++) {
         Serial.print(buffer[i] < 0x10 ? "0" : "");
