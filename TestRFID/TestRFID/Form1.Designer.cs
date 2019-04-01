25c95da7076f3d94f3c7d4dd1e8402a0d66a1808
jparedesf <49141980+jparedesf@users.noreply.github.com>
2019-04-01 01:35:19 -0500

    Add files via upload

---
+++ TestRFID/TestRFID/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TestRFID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(46, 188);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(84, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Escanear";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(206, 81);
+            this.txtCod.Multiline = true;
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(462, 216);
+            this.txtCod.TabIndex = 1;
+            this.txtCod.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Codigo RFID";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ TestRFID/TestRFID/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestRFID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Open();
+            String codigo="";
+            String rfid = "";
+
+            for (int i = 0; i < 3; i++) {           
+                rfid= serialPort1.ReadLine();
+                codigo = codigo +"\t"+rfid;
+                txtCod.Text = codigo;
+            }
+            
+            serialPort1.Close();
+        }
+    }
+}
